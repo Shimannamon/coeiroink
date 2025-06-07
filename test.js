bacacb9b871ae8783c0089787f17b746e71d17da
Shimanami <151993169+Shimannamon@users.noreply.github.com>
2025-06-07 22:28:13 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-fetch("/coeiroink/data_test.csv")
+fetch("data_test.csv")
   .then((response) => response.text())
   .then((csvText) => {
     const lines = csvText.trim().split("\n");
@@ -6,23 +6,23 @@ fetch("/coeiroink/data_test.csv")
     const tableBody = document.getElementById("tableBody");
     dataLines.forEach((line) => {
       const [id, name, age, city] = line.split(",");
-      let cityCell;
-      switch (city) {
-        case "東京":
-          cityCell = `<span class="material-symbols-outlined g">circle</span>`;
+      let nameCell;
+      switch (name) {
+        case "太郎":
+          nameCell = `<span class="material-symbols-outlined g">circle</span>`;
           break;
-        case "大阪":
-          cityCell = `<span class="material-symbols-outlined r">square</span>`;
+        case "花子":
+          nameCell = `<span class="material-symbols-outlined r">square</span>`;
           break;
         default:
-          cityCell = city;
+          nameCell = name;
       }
       const row = document.createElement("tr");
       row.innerHTML = `
               <td>${id}</td>
-              <td>${name}</td>
+              <td>${nameCell}</td>
               <td>${age}</td>
-              <td>${cityCell}</td>
+              <td>${city}</td>
             `;
       tableBody.appendChild(row);
     });
