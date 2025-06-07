5204ab226eb6bd152969bf6c6dfea0518aa35c29
Shimanami <151993169+Shimannamon@users.noreply.github.com>
2025-06-07 22:02:39 +0900

    Update test.js

--- test.js
+++ test.js
@@ -22,7 +22,7 @@ fetch("/coeiroink/data_test.csv")
               <td>${id}</td>
               <td>${name}</td>
               <td>${age}</td>
-              <td>${cityCell}</td>
+              <td>${city}</td>
             `;
       tableBody.appendChild(row);
     });
